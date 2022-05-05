96f00af853684b6961cd0f5eaa695c7fa4c82a4e
onewhoz <43013456+onewhoz@users.noreply.github.com>
2022-05-05 11:56:47 -0300

    Create Time-O(n).cs

---
+++ 167-Two-Sum II-Input-Array-Is-Sorted/Time-O(n).cs
@@ -0,0 +1,11 @@
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.Length - 1;
+        while((numbers[left] + numbers[right]) != target)
+            if ((numbers[left] + numbers[right]) > target) right--;
+            else left++;
+        
+        return new int[]{left +1, right+1};
+    }
+}
